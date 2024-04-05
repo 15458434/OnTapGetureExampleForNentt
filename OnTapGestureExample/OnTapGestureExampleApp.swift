3d11b42b1475f1ba998753f91f5d2df31fa5e14d
Mark Cornelisse <m.p.cornelisse@gmail.com>
2024-04-05 11:35:53 +0200

    Initial Commit

---
+++ OnTapGestureExample/OnTapGestureExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnTapGestureExampleApp.swift
+//  OnTapGestureExample
+//
+//  Created by Mark Cornelisse on 05/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct OnTapGestureExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
